a6ca9b6d3fd4d04959dbf8e78350239b83cc79e7
AbiramiG <abiabirami0915@gmail.com>
2026-07-23 14:32:12 +0530

    Added Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>📚 Welcome to My Book Library</h1>
+      <p>This website is successfully deployed on Render!</p>
+    </div>
+  );
+}
+
+export default Home;
